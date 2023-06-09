51380d57692f44dfc2f9fc2ba04779554dd1777b
Richi <nullnone36@gmail.com>
2023-06-09 11:46:08 -0500

    body

--- index.php
+++ index.php
@@ -2,23 +2,30 @@
 
 require 'flight/Flight.php';
 
-Flight::register('db', 'PDO', array('mysql:host=localhost;dbname=caso_23','root',''));
+Flight::register('db', 'PDO', array('mysql:host=localhost;dbname=caso_23', 'root', ''));
 
-Flight::route('GET|POST /login/@email/@password', function ($email, $password) {
-    $sql = Flight::db() -> prepare("SELECT * FROM cliente WHERE CorreoElectronico = '$email' AND contrasenia = '$password'");
-    $sql -> execute();
-    $response = $sql -> fetchAll();
+Flight::route('GET|POST /login', function () {
+    $password = (Flight::request()->data->password);
+    $email = (Flight::request()->data->email);
+
+    $sql = Flight::db()->prepare("SELECT * FROM cliente WHERE CorreoElectronico = '$email' AND contrasenia = '$password'");
+    $sql->execute();
+    $response = $sql->fetchAll();
 
     Flight::json($response);
 });
 
-Flight::route('GET|POST /new_account/@name/@last_name/@email/@password', function ($name, $last_name, $email, $password) {
-    $sql = Flight::db() -> prepare("INSERT INTO cliente (Nombre, Apellido, CorreoELectronico, contrasenia) VALUES ('$name', '$last_name', '$email', '$password')");
-    $sql -> execute();
-    $response = $sql -> fetchAll();
+Flight::route('GET|POST /new_account', function () {
+    $name = (Flight::request()->data->name);
+    $last_name = (Flight::request()->data->last_name);
+    $email = (Flight::request()->data->email);
+    $password = (Flight::request()->data->password);
+
+    $sql = Flight::db()->prepare("INSERT INTO cliente (Nombre, Apellido, CorreoELectronico, contrasenia) VALUES ('$name', '$last_name', '$email', '$password')");
+    $sql->execute();
+    $response = $sql->fetchAll();
 
     Flight::json($response);
-    Flight::redirect("/login/$email/$password");
 });
 
 
